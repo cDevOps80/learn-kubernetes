d52f7c0caf52cf37cbbb518b253d7caeee5abd1f
chaitu1 <chaitu143@gmail.com>
2024-07-30 19:56:41 +0530

    eks

--- kubectl_manifest-working/main.tf
+++ kubectl_manifest-working/main.tf
@@ -6,6 +6,6 @@ resource "null_resource" "one" {
   count = length(data.kubectl_file_documents.docs.documents)
   provisioner "local-exec" {
 
-    command = echo "${element(data.kubectl_file_documents.docs.documents, count.index)} - file"
+    command = "echo This is chaithanya"
   }
 }
