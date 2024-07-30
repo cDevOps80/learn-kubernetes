45ca9cc0446bc6ffa55987adb7f610b531aa1139
chaitu1 <chaitu143@gmail.com>
2024-07-30 13:28:14 +0530

    eks

--- eksctl/eks-module/run.sh
+++ eksctl/eks-module/run.sh
@@ -2,12 +2,12 @@ git pull
 
 terraform init
 
-if [ $1 -eq 0 ] ; then echo "Input is missing (plan|apply|destroy)!!" ; exit 1; fi
+if [ $1 -eq "0" ] ; then echo "Input is missing (plan|apply|destroy)!!" ; exit 1; fi
 
-if [ $1 = "plan" ]; then
-terraform plan
-else if [ $1 = "apply" ]; then
-terraform apply --auto-approve
-else [ $1 = "destroy" ]; then
-terraform destroy --auto-approve
-fi
+#if [ $1 = "plan" ]; then
+#terraform plan
+#else if [ $1 = "apply" ]; then
+#terraform apply --auto-approve
+#else [ $1 = "destroy" ]; then
+#terraform destroy --auto-approve
+#fi
