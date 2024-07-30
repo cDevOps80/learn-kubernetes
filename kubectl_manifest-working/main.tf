6031858844ea1cfe815ce88981558df214075f8b
chaitu1 <chaitu143@gmail.com>
2024-07-30 20:02:25 +0530

    eks

--- kubectl_manifest-working/main.tf
+++ kubectl_manifest-working/main.tf
@@ -22,23 +22,3 @@ resource "null_resource" "one" {
 }
 
 
-resource "kubectl_manifest" "test" {
-  yaml_body = <<YAML
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: test-ingress
-  annotations:
-    nginx.ingress.kubernetes.io/rewrite-target: /
-    azure/frontdoor: enabled
-spec:
-  rules:
-  - http:
-      paths:
-      - path: /testpath
-        pathType: "Prefix"
-        backend:
-          serviceName: test
-          servicePort: 80
-YAML
-}
