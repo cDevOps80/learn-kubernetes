04f2537ff42fb3931c78efd89986a8eb4389086b
chaitu1 <chaitu143@gmail.com>
2024-07-30 13:40:16 +0530

    eks

--- eksctl/eks-module/run.sh
+++ eksctl/eks-module/run.sh
@@ -2,7 +2,7 @@ git pull
 
 terraform init
 
-if [ "$1" -eq 0 ] ; then echo "Input is missing (plan|apply|destroy)!!" ; exit 1; fi
+if [ -z $1 ] ; then echo "Input is missing (plan|apply|destroy)!!" ; exit 1; fi
 
 #if [ $1 = "plan" ]; then
 #terraform plan
