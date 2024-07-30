ae279aae63374e53b5f448a2ad9133cbb0accad3
chaitu1 <chaitu143@gmail.com>
2024-07-30 20:23:38 +0530

    eks

--- kubectl_manifest-working/main.tf
+++ kubectl_manifest-working/main.tf
@@ -17,7 +17,7 @@ data "kubectl_filename_list" "manifests" {
   pattern = "sample.yaml"
 }
 
-resource "null_resource" "two" {
+resource "null_resource" "two1" {
   depends_on = [data.kubectl_filename_list.manifests]
   #count = length(data.kubectl_file_documents.docs.documents)
   count = length(data.kubectl_filename_list.manifests.matches)
