c36c372db94892dd826f81e226da3a920e4865f9
chaitu1 <chaitu143@gmail.com>
2024-07-30 14:40:54 +0530

    eks

--- eksctl/eks-module/run.sh
+++ eksctl/eks-module/run.sh
@@ -7,7 +7,7 @@ if [ -z $1 ] ; then echo "Input is missing (plan|apply|destroy)!!" ; exit 1; fi
 
 if [ $1 = "plan" ]; then
 echo terraform plan
-else if [ $1 = "apply" ]; then
+elif [ $1 = "apply" ]; then
 echo terraform apply --auto-approve
 else [ $1 = "destroy" ]
 echo terraform destroy --auto-approve
