aa0c33931f0906d71e32866c0544b6db94a579ad
chaitu1 <chaitu143@gmail.com>
2024-07-31 11:29:29 +0530

    eks

--- eksctl/eks-module/main.tf
+++ eksctl/eks-module/main.tf
@@ -40,5 +40,31 @@ resource "aws_eks_node_group" "node-group" {
     aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
     aws_iam_role_policy_attachment.example-AmazonEBSCSIDriverPolicy,
   ]
+}
+
+resource "aws_eks_node_group" "node-group1" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "roboshop-ng"
+  node_role_arn   = aws_iam_role.node-role.arn
+  subnet_ids      = ["subnet-0071e36c53f811c0b","subnet-0354194ae815795f6","subnet-0b4d667be365dede4"]
+  capacity_type   = "SPOT"
+  disk_size       = "20"
+  instance_types  = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  tags = {
+    Name = "roboshop-ng1"
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.example-AmazonEBSCSIDriverPolicy,
+  ]
 
 }
