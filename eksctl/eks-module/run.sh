65d4531fc48ca6ff99cccfae6fb16edcb1eb9ae4
chaitu1 <chaitu143@gmail.com>
2024-08-02 22:57:03 +0530

    Argocd Jobs

--- eksctl/eks-module/run.sh
+++ eksctl/eks-module/run.sh
@@ -4,5 +4,5 @@ aws eks update-kubeconfig --name roboshop
 
 sleep 15
 
-kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.33"
+#kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.33"
 
