d72478e4c47c241dd449b1e5676185ed20d9444b
chaithanya1812 <bob@example.com>
2024-09-08 10:03:57 +0530

    eks creation

--- eksctl/eks-module/run.tf
+++ eksctl/eks-module/run.tf
@@ -1,9 +1,10 @@
 resource "null_resource" "get_config" {
+  depends_on = [aws_eks_node_group.node-group1]
   provisioner "local-exec" {
     command = <<EOF
-       rm -rf ~/.kube
-       aws eks update-kubeconfig --name roboshop
-       sleep 30
+rm -rf ~/.kube
+aws eks update-kubeconfig --name roboshop
+sleep 30
 EOF
   }
 }
