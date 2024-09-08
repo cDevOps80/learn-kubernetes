4af071e77685c66a6caabcb640857842dcdc51f5
chaithanya1812 <bob@example.com>
2024-09-08 09:59:16 +0530

    eks creation

--- eksctl/eks-module/main.tf
+++ eksctl/eks-module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "eks" {
   name     = "roboshop"
   role_arn = aws_iam_role.eks-role.arn
-  version = 1.28
+ # version = 1.28
 
   vpc_config {
     subnet_ids = ["subnet-0071e36c53f811c0b","subnet-0354194ae815795f6"]
